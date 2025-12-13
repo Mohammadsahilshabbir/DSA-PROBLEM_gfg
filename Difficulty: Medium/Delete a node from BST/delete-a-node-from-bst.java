95557622d80c54f1c9c9197a39be48ea32ea1c5e
Mohammadsahilshabbir <sahilshabbir4568@gmail.com>
2025-12-13 14:09:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Delete a node from BST/delete-a-node-from-bst.java
@@ -0,0 +1,48 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public Node delNode(Node root, int x) {
+        // code here
+        if(root==null) return null;
+        if(x<root.data){
+            root.left=delNode(root.left,x);
+        }else if(x>root.data){
+            root.right=delNode(root.right,x);
+        }else{
+            if(root.left==null&&root.right==null){
+                return null;
+            }
+            else if(root.left==null){
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }
+            else {
+                Node successor = findMin(root.right);
+                root.data = successor.data;
+                root.right = delNode(root.right, successor.data);
+            }
+
+        }
+            return root;
+    }
+
+    // Helper function to find the minimum value node in a subtree
+    private Node findMin(Node node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+
+    }
+}
