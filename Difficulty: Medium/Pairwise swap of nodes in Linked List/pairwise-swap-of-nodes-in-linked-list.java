f22dd6921668762c108b9e32e29ed1b11acc9288
Mohammadsahilshabbir <sahilshabbir4568@gmail.com>
2026-01-05 20:22:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pairwise swap of nodes in Linked List/pairwise-swap-of-nodes-in-linked-list.java
@@ -0,0 +1,13 @@
+class Solution {
+    public Node pairwiseSwap(Node head) {
+        Node curr=head;
+        while(curr!=null&&curr.next!=null){
+            int temp=curr.data;
+            curr.data=curr.next.data;
+            curr.next.data=temp;
+            
+            curr=curr.next.next;
+        }
+        return head;
+    }
+}
