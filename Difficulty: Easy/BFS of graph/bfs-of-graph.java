b38aa1f65b6bffc73533012275400a5ebc071b34
Mohammadsahilshabbir <sahilshabbir4568@gmail.com>
2026-01-16 01:26:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -3,29 +3,24 @@ class Solution {
         // code here
         ArrayList<Integer> result = new ArrayList<>();
 
-        int V = adj.size(); 
+        int V=adj.size();
+        boolean[] visited=new boolean[V];
+        Queue<Integer> queue=new LinkedList<>();
         
-        boolean[] visited = new boolean[V];
-        Queue<Integer> queue = new LinkedList<>();
         
-        
-        visited[0] = true;
+        visited[0]=true;
         queue.add(0);
-        
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
+        while(!queue.isEmpty()){
+            int node=queue.poll();
             result.add(node);
             
-            
-            for (int neighbor : adj.get(node)) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
+            for(int neighbor:adj.get(node)){
+                if(!visited[neighbor]){
+                    visited[neighbor]=true;
                     queue.add(neighbor);
                 }
             }
         }
-        
         return result;
-
     }
 }
