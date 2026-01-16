c003153c4b386598b09c77ba472cd693e7053aee
Mohammadsahilshabbir <sahilshabbir4568@gmail.com>
2026-01-16 22:18:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Arrays with Distinct/intersection-of-arrays-with-distinct.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int intersectSize(int a[], int b[]) {
+        // Your code here
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : a) {
+            set.add(num);
+        }
+        
+        
+        int count = 0;
+        for (int num : b) {
+            if (set.contains(num)) {
+                count++;
+                
+                set.remove(num);
+            }
+        }
+        
+        return count;
+
+    }
+}
