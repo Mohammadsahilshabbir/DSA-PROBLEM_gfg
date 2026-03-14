da46a9ae57abea8de85b0aea812a90ab19461cab
Mohammadsahilshabbir <sahilshabbir4568@gmail.com>
2026-03-14 12:33:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -2,37 +2,55 @@ class Solution {
     ArrayList<Integer> find(int arr[], int x) {
         // code here
         ArrayList<Integer> ans=new ArrayList<>();
+        // int n=arr.length;
+        // int low=0,high=n-1,idx1=-1,idx2=-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(arr[mid]==x){
+        //         idx1=mid;
+        //         high=mid-1;
+        //     }
+        //     else if(arr[mid]<x){
+        //         low=mid+1;
+        //     }else{
+        //         high=mid-1;
+        //     }
+        // }
+        // low=0;
+        // high=n-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(arr[mid]==x){
+        //         idx2=mid;
+        //         low=mid+1;
+        //     }
+        //     else if(arr[mid]<x){
+        //         low=mid+1;
+        //     }else{
+        //         high=mid-1;
+        //     }
+        // }
+        // ans.add(idx1);
+        // ans.add(idx2);
+        
+        // return ans;
+        int idx1=-1;
+        int idx2=-1;
         int n=arr.length;
-        int low=0,high=n-1,idx1=-1,idx2=-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]==x){
-                idx1=mid;
-                high=mid-1;
-            }
-            else if(arr[mid]<x){
-                low=mid+1;
-            }else{
-                high=mid-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==x) {
+                idx1=i;
+                break;
             }
         }
-        low=0;
-        high=n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]==x){
-                idx2=mid;
-                low=mid+1;
-            }
-            else if(arr[mid]<x){
-                low=mid+1;
-            }else{
-                high=mid-1;
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]==x) {
+                idx2=i;
+                break;
             }
         }
         ans.add(idx1);
         ans.add(idx2);
-        
         return ans;
     }
 }
